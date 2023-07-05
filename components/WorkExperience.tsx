3944990c1a2ed5fcdc9c2ac19c58cb00ccd6b40c
Sathwic97 <sathwic97@gmail.com>
2023-07-05 12:19:06 +0530

    updated information

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -27,24 +27,19 @@ export default function WorkExperience({  }: Props) {
       <div className='w-screen mt-16 h-[72%] flex space-x-5 scrollbar-track-gray-400/20 scrollbar-thumb-[#39FF14]/80 scrollbar-thin overflow-x-auto p-10 snap-x snap-mandatory'>
 
 
-<ExperienceCard role='React Trainee' companyName='BRN Infotech' companyImage='/company-logos/brninfotech.png' started='2022-05-01' ended='2022-10-30'
+<ExperienceCard role='React Developer' companyName='Cureus Technologies Private Limited' companyImage='/company-logos/cureus.png' started='2021-10' ended='present'
 
 point1='Translated designs into clean, efficient, and maintainable code and gained experience on version control systems like Git and debugging tools like Jest'
 point2='Contributed to the development of responsive and accessible user interfaces using ReactJS'
 point3='Worked with RESTful APIs and Asynchoronous programming to fetch and manipulate data for use in ReactJS applications'
 />
-<ExperienceCard role='Lumos Wizard' companyImage='/company-logos/lumoswizards.png' companyName='Lumos Labs - Metaverse'  started='2023-02-01' ended='present'
+<ExperienceCard role='Lumos Wizard' companyImage='/company-logos/lumoswizards.png' companyName='Lumos Labs - Metaverse'  started='2023-02' ended='present'
 
 point1='Web3 centric social media content design and writing.'
 point2='Underwent various educational sessions regarding the web3 ecosystem'
 point3='Gained experience on content design and content writing as well as developed good communication skills resulting in being able to play the role of a better team-player and a strategic contributor'
 />
-<ExperienceCard role='Content Manager Trainee' companyName='Salman Needs A Job' companyImage='/company-logos/salman.jpg' started='2022-12-01' ended='2023-01-31'
 
-point1='Managed YouTube channel, including uploading, publishing, and promoting videos to increase reach and engagement'
-point2='Optimized content for search engine optimization (SEO) and user engagement, including title, description, tags, and thumbnail images'
-point3='Conducted research on web3 technologies'
-/>
 
 
 
